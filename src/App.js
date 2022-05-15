927d9f4c6c5a1af6e74c2044b01288fb9e4cefc6
AhmedKvz <86858400+AhmedKvz@users.noreply.github.com>
2022-05-16 00:06:29 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./styles/App.css";
-import Header from "./components/Header";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import { useEffect } from "react";
+import { useStateValue } from "./services/StateProvider";
+import { auth } from "./services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      console.log("the user is", currentUser);
+
+      if (currentUser) {
+        dispatch({
+          type: "SET_USER",
+          user: currentUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
