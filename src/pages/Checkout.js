3c239b508ac1ad7e01e490aee7905b2f292d3a3c
AhmedKvz <86858400+AhmedKvz@users.noreply.github.com>
2022-05-16 00:05:15 +0200

    Add files via upload

---
+++ src/pages/Checkout.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Subtotal from "../components/Subtotal";
+import "../styles/Checkout.css";
+import { useStateValue } from "../services/StateProvider";
+import CheckoutProduct from "../components/CheckoutProduct";
+import Header from "../components/Header";
+
+function Checkout() {
+  const [{ basket, user }] = useStateValue();
+
+  return (
+    <>
+      <Header />
+      <div className="checkout">
+        <div className="checkout__header">
+          <div className="checkout__left">
+            <h4>Hello, {user?.email}</h4>
+            <h2 className="checkout__title">Your shopping basket</h2>
+          </div>
+          <div className="checkout__right">
+            <Subtotal />
+          </div>
+        </div>
+
+        <hr />
+        <div className="checkout__products">
+          {basket.map((item) => (
+            <CheckoutProduct
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Checkout;
---
+++ src/pages/Login.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "../styles/Login.css";
+import Transparent from "../images/Transparent.png";
+import { Link } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
+  const navigate = useNavigate();
+
+  const signIn = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        signInEmail,
+        signInPassword
+      );
+      console.log(user);
+      if (user) {
+        navigate("/");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+      if (user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="login">
+      <Link to={"/"}>
+        <img className="login__logo" src={Transparent} alt="transparent.jpg" />
+      </Link>
+      <div className="login__form">
+        <h1>Sign-in</h1>
+        <form className="signIn__form">
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            placeholder="...enter your email"
+            value={signInEmail}
+            onChange={(e) => setSignInEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            placeholder="...enter your password"
+            value={signInPassword}
+            onChange={(e) => setSignInPassword(e.target.value)}
+          />
+          <div className="btn__wrapper">
+            <button type="submit" onClick={signIn} className="login_signIn_btn">
+              Sign-in
+            </button>
+
+            <div className="register__form">
+              <h5>Don't have an account?</h5>
+              <input
+                type="text"
+                placeholder="register your email"
+                value={registerEmail}
+                onChange={(e) => setRegisterEmail(e.target.value)}
+              />
+              <h5>Password</h5>
+              <input
+                type="password"
+                placeholder="...enter your password"
+                value={registerPassword}
+                onChange={(e) => setRegisterPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={register}
+              className="login_register_btn"
+            >
+              Create account
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
